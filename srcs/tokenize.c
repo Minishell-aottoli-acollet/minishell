63b637201c9f9b451e0723a9a73d08ddcb8f3147
aottolin <alexisottolini@MacBook-Pro-de-Alexis.local>
2024-11-24 18:28:50 +0100

    delete lines in tokenize.c

--- srcs/tokenize.c
+++ srcs/tokenize.c
@@ -1,13 +1,3 @@
 # include "minishell.h"
 
-
-t_token *tokenize(void)
-{
-	char	*input_tmp;
-	t_token	*tokens_input;
-
-	input_tmp = t_minishell.input;
-	tokens_input = handler_tokenize(input_tmp);
-	return (tokens_input);
-
-}
+//TO do 
